3db5bb582bf9bc28a3984f40f3431b179cf55c97
deglingo <alfredeneumeuh@laposte.net>
2014-02-05 15:09:07 +0100

    mbclean: parsed the command line

--- mybackup/mbclean.py
+++ mybackup/mbclean.py
@@ -1,7 +1,22 @@
 #
 
+import sys, getopt, logging
+
 from mybackup.log import *
 from mybackup.config import Config
+
+
+# USAGE:
+#
+USAGE = """\
+USAGE: mbclean [OPTIONS] CONFIG
+
+OPTIONS:
+
+  -q, --quiet   be less verbose
+  -v, --verbose be more verbose
+  -h, --help    print this message and exit
+"""
 
 
 # MBCleanApp:
@@ -15,17 +30,38 @@ class MBCleanApp :
         self.__log_setup()
         self.config = Config()
         # parse the command line
+        shortopts = 'qvh'
+        longopts = ['quiet', 'verbose', 'help']
+        opts, args = getopt.gnu_getopt(sys.argv[1:], shortopts, longopts)
+        for o, a in opts :
+            if o in ('-h', '--help') :
+                sys.stdout.write(USAGE)
+                sys.exit(0)
+            elif o in ('-q', '--quiet') :
+                self.config.verb_level -= 1
+            elif o in ('-v', '--verbose') :
+                self.config.verb_level += 1
+            else :
+                assert 0, (o, a)
+        # fix log level
+        self.log_cfilter.enable(logging.DEBUG,    self.config.verb_level >= 3)
+        self.log_cfilter.enable(logging.INFO,     self.config.verb_level >= 2)
+        self.log_cfilter.enable(logging.WARNING,  self.config.verb_level >= 1)
+        self.log_cfilter.enable(logging.ERROR,    self.config.verb_level >= 1)
+        self.log_cfilter.enable(logging.CRITICAL, self.config.verb_level >= 0)
+        # init the config
+        assert len(args) == 1, args
+        self.config.init(args.pop(0))
 
 
     # __log_setup:
     #
     def __log_setup (self) :
         logger = log_setup('mbclean')
-        self.cfilt = LogLevelFilter()
+        self.log_cfilter = LogLevelFilter()
         hdlr = LogConsoleHandler()
-        hdlr.addFilter(self.cfilt)
+        hdlr.addFilter(self.log_cfilter)
         logger.addHandler(hdlr)
-        trace("HELLO!")
 
 
 # exec
