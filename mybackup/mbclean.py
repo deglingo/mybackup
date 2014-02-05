c6026daf3f9657928a952e26d9d303a79fce65bd
deglingo <alfredeneumeuh@laposte.net>
2014-02-05 14:18:39 +0100

    added mbclean

---
+++ mybackup/mbclean.py
@@ -0,0 +1,18 @@
+#
+
+
+# MBCleanApp:
+#
+class MBCleanApp :
+
+
+    # main:
+    #
+    def main (self) :
+        pass
+
+
+# exec
+if __name__ == '__main__' :
+    app = MBCleanApp()
+    app.main()
